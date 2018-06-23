b336b973843b7f178868bc527a59bbb9a9eeb313
Charlotte Soneson <charlottesoneson@gmail.com>
2018-06-23 17:57:33 +0200

    Add Kumar data sets to make-metadata.R

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -232,7 +232,242 @@ df_res_filteredM3Drop10_KohTCC <- data.frame(
   stringsAsFactors = FALSE
 )
 
-# Combined meta-data
+## ------------------------- filteredExpr10_Kumar --------------------------- ##
+df_sce_filteredExpr10_Kumar <- data.frame(
+  Title = "sce_filteredExpr10_Kumar",
+  Description = "scRNA-seq of mESCs with various genetic perturbations, cultured in different medias. Select genes by expression level.",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredExpr10_Kumar.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredExpr10_Kumar <- data.frame(
+  Title = "clustering_summary_filteredExpr10_Kumar",
+  Description = "Clustering results for sce_filteredExpr10_Kumar",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredExpr10_Kumar.rds",
+  stringsAsFactors = FALSE
+)
+
+## ------------------------- filteredHVG10_Kumar ---------------------------- ##
+df_sce_filteredHVG10_Kumar <- data.frame(
+  Title = "sce_filteredHVG10_Kumar",
+  Description = "scRNA-seq of mESCs with various genetic perturbations, cultured in different media. Select highly variable genes.",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredHVG10_Kumar.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredHVG10_Kumar <- data.frame(
+  Title = "clustering_summary_filteredHVG10_Kumar",
+  Description = "Clustering results for sce_filteredHVG10_Kumar",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredHVG10_Kumar.rds",
+  stringsAsFactors = FALSE
+)
+
+## ------------------------ filteredM3Drop10_Kumar -------------------------- ##
+df_sce_filteredM3Drop10_Kumar <- data.frame(
+  Title = "sce_filteredM3Drop10_Kumar",
+  Description = "scRNA-seq of mESCs with various genetic perturbations, cultured in different media. Genes selected using the M3Drop package.",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredM3Drop10_Kumar.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredM3Drop10_Kumar <- data.frame(
+  Title = "clustering_summary_filteredM3Drop10_Kumar",
+  Description = "Clustering results for sce_filteredM3Drop10_Kumar",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredM3Drop10_Kumar.rds",
+  stringsAsFactors = FALSE
+)
+
+## ----------------------- filteredExpr10_KumarTCC -------------------------- ##
+df_sce_filteredExpr10_KumarTCC <- data.frame(
+  Title = "sce_filteredExpr10_KumarTCC",
+  Description = "scRNA-seq of mESCs with various genetic perturbations, cultured in different media. Select genes by expression level.",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredExpr10_KumarTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredExpr10_KumarTCC <- data.frame(
+  Title = "clustering_summary_filteredExpr10_KumarTCC",
+  Description = "Clustering results for sce_filteredExpr10_KumarTCC",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredExpr10_KumarTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+## ----------------------- filteredHVG10_KumarTCC --------------------------- ##
+df_sce_filteredHVG10_KumarTCC <- data.frame(
+  Title = "sce_filteredHVG10_KumarTCC",
+  Description = "scRNA-seq of mESCs with various genetic perturbations, cultured in different media. Select highly variable genes.",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredHVG10_KumarTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredHVG10_KumarTCC <- data.frame(
+  Title = "clustering_summary_filteredHVG10_KumarTCC",
+  Description = "Clustering results for sce_filteredHVG10_KumarTCC",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredHVG10_KumarTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+## ---------------------- filteredM3Drop10_KumarTCC ------------------------- ##
+df_sce_filteredM3Drop10_KumarTCC <- data.frame(
+  Title = "sce_filteredM3Drop10_KumarTCC",
+  Description = "scRNA-seq of mESCs with various genetic perturbations, cultured in different media. Genes selected using the M3Drop package.",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredM3Drop10_KumarTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredM3Drop10_KumarTCC <- data.frame(
+  Title = "clustering_summary_filteredM3Drop10_KumarTCC",
+  Description = "Clustering results for sce_filteredM3Drop10_KumarTCC",
+  BiocVersion = "3.7",
+  Genome = "GRCm38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Mus musculus",
+  TaxonomyId = "",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredM3Drop10_KumarTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+
+## Combined meta-data
 df_all <- rbind(
   df_sce_filteredExpr10_Koh,
   df_sce_filteredHVG10_Koh,
@@ -245,8 +480,20 @@ df_all <- rbind(
   df_res_filteredM3Drop10_Koh,
   df_res_filteredExpr10_KohTCC,
   df_res_filteredHVG10_KohTCC,
-  df_res_filteredM3Drop10_KohTCC
+  df_res_filteredM3Drop10_KohTCC,
+  df_sce_filteredExpr10_Kumar,
+  df_sce_filteredHVG10_Kumar,
+  df_sce_filteredM3Drop10_Kumar,
+  df_sce_filteredExpr10_KumarTCC,
+  df_sce_filteredHVG10_KumarTCC,
+  df_sce_filteredM3Drop10_KumarTCC,
+  df_res_filteredExpr10_Kumar,
+  df_res_filteredHVG10_Kumar,
+  df_res_filteredM3Drop10_Kumar,
+  df_res_filteredExpr10_KumarTCC,
+  df_res_filteredHVG10_KumarTCC,
+  df_res_filteredM3Drop10_KumarTCC
 )
 
-# Save .csv file
+## Save .csv file
 write.csv(df_all, file = "../extdata/metadata.csv", row.names = FALSE)
