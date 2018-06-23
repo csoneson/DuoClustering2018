de463b7587aecdc66b252bf34f1b4e6bd4b2365a
Charlotte Soneson <charlottesoneson@gmail.com>
2018-06-23 17:49:53 +0200

    Add make-metadata.R

---
+++ inst/scripts/make-metadata.R
@@ -0,0 +1,252 @@
+## -------------------------- filteredExpr10_Koh ---------------------------- ##
+df_sce_filteredExpr10_Koh <- data.frame(
+  Title = "sce_filteredExpr10_Koh",
+  Description = "scRNA-seq of in vitro cultured H7 embryonic stem cells (WiCell) and H7-derived downstream early mesoderm progenitors. Select genes by expression level.",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredExpr10_Koh.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredExpr10_Koh <- data.frame(
+  Title = "clustering_summary_filteredExpr10_Koh",
+  Description = "Clustering results for sce_filteredExpr10_Koh",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredExpr10_Koh.rds",
+  stringsAsFactors = FALSE
+)
+
+## -------------------------- filteredHVG10_Koh ----------------------------- ##
+df_sce_filteredHVG10_Koh <- data.frame(
+  Title = "sce_filteredHVG10_Koh",
+  Description = "scRNA-seq of in vitro cultured H7 embryonic stem cells (WiCell) and H7-derived downstream early mesoderm progenitors. Select highly variable genes.",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredHVG10_Koh.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredHVG10_Koh <- data.frame(
+  Title = "clustering_summary_filteredHVG10_Koh",
+  Description = "Clustering results for sce_filteredHVG10_Koh",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredHVG10_Koh.rds",
+  stringsAsFactors = FALSE
+)
+
+## ------------------------- filteredM3Drop10_Koh --------------------------- ##
+df_sce_filteredM3Drop10_Koh <- data.frame(
+  Title = "sce_filteredM3Drop10_Koh",
+  Description = "scRNA-seq of in vitro cultured H7 embryonic stem cells (WiCell) and H7-derived downstream early mesoderm progenitors. Genes selected using the M3Drop package.",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredM3Drop10_Koh.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredM3Drop10_Koh <- data.frame(
+  Title = "clustering_summary_filteredM3Drop10_Koh",
+  Description = "Clustering results for sce_filteredM3Drop10_Koh",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredM3Drop10_Koh.rds",
+  stringsAsFactors = FALSE
+)
+
+## ------------------------ filteredExpr10_KohTCC --------------------------- ##
+df_sce_filteredExpr10_KohTCC <- data.frame(
+  Title = "sce_filteredExpr10_KohTCC",
+  Description = "scRNA-seq of in vitro cultured H7 embryonic stem cells (WiCell) and H7-derived downstream early mesoderm progenitors. Select genes by expression level.",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredExpr10_KohTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredExpr10_KohTCC <- data.frame(
+  Title = "clustering_summary_filteredExpr10_KohTCC",
+  Description = "Clustering results for sce_filteredExpr10_KohTCC",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredExpr10_KohTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+## ------------------------ filteredHVG10_KohTCC ---------------------------- ##
+df_sce_filteredHVG10_KohTCC <- data.frame(
+  Title = "sce_filteredHVG10_KohTCC",
+  Description = "scRNA-seq of in vitro cultured H7 embryonic stem cells (WiCell) and H7-derived downstream early mesoderm progenitors. Select highly variable genes.",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredHVG10_KohTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredHVG10_KohTCC <- data.frame(
+  Title = "clustering_summary_filteredHVG10_KohTCC",
+  Description = "Clustering results for sce_filteredHVG10_KohTCC",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredHVG10_KohTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+## ----------------------- filteredM3Drop10_KohTCC -------------------------- ##
+df_sce_filteredM3Drop10_KohTCC <- data.frame(
+  Title = "sce_filteredM3Drop10_KohTCC",
+  Description = "scRNA-seq of in vitro cultured H7 embryonic stem cells (WiCell) and H7-derived downstream early mesoderm progenitors. Genes selected using the M3Drop package.",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = "FASTQ",
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "SingleCellExperiment",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/sce_filteredM3Drop10_KohTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+df_res_filteredM3Drop10_KohTCC <- data.frame(
+  Title = "clustering_summary_filteredM3Drop10_KohTCC",
+  Description = "Clustering results for sce_filteredM3Drop10_KohTCC",
+  BiocVersion = "3.7",
+  Genome = "GRCh38",
+  SourceType = NA,
+  SourceUrl = "",
+  SourceVersion = NA,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = TRUE,
+  DataProvider = "",
+  Maintainer = "Charlotte Soneson <charlottesoneson@gmail.com>",
+  RDataClass = "data.frame",
+  DispatchClass = "rds",
+  RDataPath = "DuoClustering2018/clustering_summary_filteredM3Drop10_KohTCC.rds",
+  stringsAsFactors = FALSE
+)
+
+# Combined meta-data
+df_all <- rbind(
+  df_sce_filteredExpr10_Koh,
+  df_sce_filteredHVG10_Koh,
+  df_sce_filteredM3Drop10_Koh,
+  df_sce_filteredExpr10_KohTCC,
+  df_sce_filteredHVG10_KohTCC,
+  df_sce_filteredM3Drop10_KohTCC,
+  df_res_filteredExpr10_Koh,
+  df_res_filteredHVG10_Koh,
+  df_res_filteredM3Drop10_Koh,
+  df_res_filteredExpr10_KohTCC,
+  df_res_filteredHVG10_KohTCC,
+  df_res_filteredM3Drop10_KohTCC
+)
+
+# Save .csv file
+write.csv(df_all, file = "../extdata/metadata.csv", row.names = FALSE)
