a4e3462b3609fefa1606aa2dc09da151927ea46a
Charlotte Soneson <charlottesoneson@gmail.com>
2018-07-13 14:31:48 +0200

    Fix metadata for simulated and Zheng data sets

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
+#' importFrom utils read.csv
 .onLoad <- function(libname, pkgname) {
   fl <- system.file("extdata", "metadata.csv", package = "DuoClustering2018")
   titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-  createHubAccessors(pkgname, titles)
+  ExperimentHub::createHubAccessors(pkgname, titles)
 }
