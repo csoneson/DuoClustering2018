baec69cf53b9fad517d4862aa8dc7de3bcfcb9ca
Charlotte Soneson <charlottesoneson@gmail.com>
2018-08-03 20:06:29 +0200

    Accessor functions for data sets

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,5 @@
 .onLoad <- function(libname, pkgname) {
   fl <- system.file("extdata", "metadata.csv", package = "DuoClustering2018")
   titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-  titles <- setdiff(titles, grep("sce", titles, value = TRUE))
   ExperimentHub::createHubAccessors(pkgname, titles)
 }
